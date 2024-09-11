34ebbdead46de22c39a84cba954f1631ec51ad82
suyashmhetre <mhetresuyash676@gmail.com>
2024-09-11 21:14:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-let equal = document.getElementById("hours");
-let equalto = document.getElementById("equal"); 
-let prod  = document.getElementById("dot");
-let six = document.getElementById("submit");
-let nine = document.getElementById("submits");
-let nine2 = document.getElementById("nine");
-let star = document.getElementById("dote");
-let display = document.getElementById("display");
 
-function appendToDisplay(input) {
-     display.value += input
-}
-function calculate() {
-    display.value = eval(display.value)
-}
+let answer = 	document.getElementById('currentTime');
 
-function clearDisplay() {
-     display.value = ""
-}
+let input = 	document.getElementById('bill');
+let input2 = 	document.getElementById('billp');
 
+
+let btn = document.getElementById('submit');
+
+btn.addEventListener("click",()=>{
+  
+   let value = input.value;
+   let value2 = (input2.value);
+   let z = value - (value2 * 100)
+   let w = ((value2 * 100) * 5.16) + (z * 11.09) + 128 + (value * 1.17); 
+   if (value >= 0 && value <= 100) {
+     answer.textContent = value * 4.71
+   }
+   if (value >= 101 && value <= 300) {
+    
+    let billans =  ((value2 * 100) * 5.16) + (z * 11.09) + 128 + (value * 1.17) + (w * 0.16);
+    answer.textContent = billans
+   }
+});
